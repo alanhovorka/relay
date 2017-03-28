06bc62954b2cab087f0d2ef70c47ec1efa669663
alanhovorka <alanhovorka94@gmail.com>
2017-03-28 11:41:15 -0400

    Creating templates

---
+++ template.js
@@ -0,0 +1,11 @@
+
+// external js: masonry.pkgd.js
+
+var msnry = new Masonry( '.grid', {
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true,
+  stagger: 10
+});
+
+
